32d9e3b4fd5eb1cd79d8633012fdd2f24d821b00
purplefox1977 <141675561+purplefox1977@users.noreply.github.com>
2023-08-14 16:01:35 -0300

    Add files via upload

---
+++ FormularioLogin/src/main/java/com/mycompany/formulariologin/FormularioLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.formulariologin;
+
+/**
+ *
+ * @author Loboplateado77
+ */
+public class FormularioLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
